a27ce31a76482c95d3b0627de919c110452515a5
Márton Boros <martonboros@gmail.com>
2015-06-19 18:40:22 +0000

    add user nixpkgs

---
+++ .nixpkgs/config.nix
@@ -0,0 +1,24 @@
+{
+  allowUnfree = true;
+  chromium = {
+    enablePepperFlash = true;
+    enablePepperPDF = true;
+  };
+
+
+  packageOverrides = pkgs_: with pkgs_; {
+    my_ = with pkgs; buildEnv {
+      name = "my_";
+      paths = [
+        i3 i3status i3lock dmenu compton
+        zsh fish bc rxvt_unicode evilvte xfce.terminal
+        sublime3 textadept
+        mc htop psmisc
+        feh geeqie scrot
+        nox
+        chromium firefox
+        nodejs
+      ];
+    };
+  };
+}
