1fe2916b50b7dbb87d032ffb5a6c60a582b53679
Márton Boros <martonboros@gmail.com>
2016-06-12 05:59:31 +0200

    add backlight

--- .nixpkgs/config.nix
+++ .nixpkgs/config.nix
@@ -10,14 +10,14 @@
     my_ = with pkgs; buildEnv {
       name = "my_";
       paths = [
-        i3 i3status i3lock dmenu compton
+        i3 i3status i3lock dmenu compton xbacklight
         zsh fish bc rxvt_unicode evilvte xfce.terminal
         sublime3 textadept
         mc htop psmisc
         feh geeqie scrot
         nox
         chromium firefox
-        nodejs
+        # nodejs
       ];
     };
   };
