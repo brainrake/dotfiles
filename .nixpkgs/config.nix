77c1a680e0d47ae74b036207ca9652abde851ae2
Márton Boros <martonboros@gmail.com>
2016-06-24 17:25:43 +0200

    update nixpkgs config

--- .nixpkgs/config.nix
+++ .nixpkgs/config.nix
@@ -4,24 +4,21 @@
     enablePepperFlash = true;
     enablePepperPDF = true;
   };
-
-
   packageOverrides = pkgs_: with pkgs_; {
     my_ = with pkgs; buildEnv {
       name = "my_";
       paths = [
-        i3 i3status i3lock dmenu compton xorg.xbacklight volumeicon xorg.xev hsetroot
-        arandr glxinfo
-        zsh rxvt_unicode bc
-        git
-        sublime3 textadept atom
-        mc htop psmisc pcmanfm
-        feh geeqie scrot
-        nox
-        chromium firefox
-        transmission_gtk
-        mpv
-        # nodejs
+        i3 i3status i3lock dmenu
+        xorg.xbacklight xorg.xev hsetroot glxinfo volumeicon xorg.xev compton
+        rxvt_unicode feh geeqie scrot arandr lxappearance mpv pcmanfm gcolor2
+        chromium firefox thunderbird
+        # transmission-gtk seahorse
+        sublime3 textadept atom mixxx
+        git gcc racket
+        haskellPackages.purescript  # haskellPackages.idris
+        nodejs nodePackages.coffee-script
+        iputils bind nmap mtr terraform  # urbit
+        nox bc mc htop psmisc file graphviz imagemagick pciutils gnupg
       ];
     };
   };
