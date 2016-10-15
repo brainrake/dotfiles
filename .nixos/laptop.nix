28c0155a567757801f40499389a83159dc2697a8
Márton Boros <martonboros@gmail.com>
2016-10-15 10:43:55 +0200

    update and split nixos config

---
+++ .nixos/laptop.nix
@@ -0,0 +1,19 @@
+{ config, pkgs, ... }:
+{
+  imports = [ ./common.nix ];
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.initrd.luks.devices = [ { device = "/dev/nvme0n1p6"; name = "ct"; } ];
+
+  fileSystems."/".device = "/dev/ct/root";
+  fileSystems."/".options = [ "defaults" "noatime" ];
+  fileSystems."/boot".device = "/dev/nvme0n1p1";
+  fileSystems."/w" = {
+    device = "/dev/nvme0n1p3";
+    options = [ "ro" "defaults" "umask=000" "noatime" ];
+    fsType = "ntfs-3g";
+  };
+
+  networking.hostName = "eki";
+}
