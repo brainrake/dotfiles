1015ebf2b46c5191aabcb79c95eb48aa745d1cd9
Márton Boros <martonboros@gmail.com>
2017-01-28 15:21:44 +0100

    update packages

--- .nixpkgs/config.nix
+++ .nixpkgs/config.nix
@@ -2,22 +2,41 @@
   allowUnfree = true;
   chromium.enablePepperFlash = true;
   chromium.enablePepperPDF = true;
-  # chromium.jre = true;
-  # firefox.jre = true;
-  packageOverrides = pkgs_: with pkgs_; {
-    my_ = with pkgs; buildEnv {
+  packageOverrides = pkgs: rec {
+    my_ = pkgs.buildEnv {
       name = "my_";
-      paths = [
-        i3 i3status i3lock dmenu
-        xorg.xbacklight xorg.xev xorg.xkbcomp xorg.xmodmap hsetroot glxinfo volumeicon xorg.xev compton kdeconnect
-        rxvt_unicode feh geeqie scrot arandr lxappearance mpv pcmanfm gcolor2 gimp evince paprefs pavucontrol
-        chromium google-chrome firefox thunderbird
-        transmission_gtk gnome3.seahorse
-        sublime3 textadept mixxx # atom
+      paths = [ my_dev ];
+    };
+    my_dev = pkgs.buildEnv {
+      name = "my_dev";
+      paths = [ my_desktop ] ++ (with pkgs; [
+        gnome3.seahorse
+        google-chrome firefox
         git gcc racket elmPackages.elm nix-repl direnv meld file graphviz imagemagick
         haskellPackages.purescript haskellPackages.idris
         nodejs nodePackages.coffee-script
-        iputils bind nmap mtr iptraf wget gnupg mkpasswd terraform  # urbit
+        sublime3 textadept atom
+        mixxx
+        gimp
+        terraform
+        urbit
+      ]);
+    };
+    my_desktop = pkgs.buildEnv {
+      name = "my_desktop";
+      paths = [ my_tools ] ++ (with pkgs; [
+        i3 i3status i3lock dmenu
+        chromium # thunderbird
+        xorg.xbacklight xorg.xev xorg.xkbcomp xorg.xmodmap hsetroot glxinfo volumeicon xorg.xev compton kdeconnect
+        rxvt_unicode feh geeqie scrot arandr lxappearance mpv pcmanfm gcolor2 evince paprefs pavucontrol
+        transmission_gtk
+        pidgin pidginotr pidginlatex purple-hangouts purple-plugin-pack toxprpl
+      ]);
+    };
+    my_tools = pkgs.buildEnv {
+      name = "my_tools";
+      paths = with pkgs; [
+        iputils bind nmap mtr iptraf wget gnupg mkpasswd
         nox bc mc htop psmisc pciutils lm_sensors
       ];
     };
