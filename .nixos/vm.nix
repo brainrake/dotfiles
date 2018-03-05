d398763b9e33c63e636f162e0a8f7e48804ab358
Márton Boros <martonboros@gmail.com>
2018-03-05 11:16:57 +0100

    fix typo

--- .nixos/vm.nix
+++ .nixos/vm.nix
@@ -1,6 +1,6 @@
 { config, pkgs, ... }:
 {
-  imports = [ ./common.nix ./grapgical.nix ];
+  imports = [ ./common.nix ./graphical.nix ];
 
   boot.loader.grub.enable = true;
   boot.loader.grub.version = 2;
