ad8078f681cf590941ff5ea5a10e11d79333891d
Márton Boros <martonboros@gmail.com>
2018-04-09 13:04:10 +0200

    update calculon to nixos 18.03

--- .nixos/calculon.nix
+++ .nixos/calculon.nix
@@ -13,10 +13,10 @@
     hostName = "calculon";
     domain = "newt";
     nameservers = [ "8.8.8.8" "8.8.4.4" ];
-    interfaces.enp1s0 = {
-      ipAddress = "192.168.100.10"; 
+    interfaces.enp1s0.ipv4.addresses = [ {
+      address = "192.168.100.10";
       prefixLength = 24;
-    };
+    } ];
   };
 
   services.dhcpd4 = {
